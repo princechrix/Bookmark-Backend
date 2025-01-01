019afa77bf43395c21cbc6677a157b70023a2c60
princechrix <princechrix.dev@gmail.com>
2025-01-01 10:01:57 +0200

    ''

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+// import { User, Bookmark } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signup() {
-    return {msg: 'I have signed up'}
+    return { msg: 'I have signed up' };
   }
   signin() {
-    return {msg: "I have signed in"}
+    return { msg: 'I have signed in' };
   }
 }
